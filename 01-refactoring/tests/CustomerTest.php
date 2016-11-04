4c6a2abc74903f46997a7c5ce7a012eea936f33b
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2016-11-04 21:25:41 +0100

    01: Testing Autoload Fixed

--- 01-refactoring/tests/CustomerTest.php
+++ 01-refactoring/tests/CustomerTest.php
@@ -1,9 +1,8 @@
 <?php
 
-require_once(dirname(__DIR__).'/Customer.php');
-require_once(dirname(__DIR__).'/Rental.php');
-require_once(dirname(__DIR__).'/Movie.php');
+namespace Refactoring;
 
+use PHPUnit_Framework_TestCase;
 
 class CustomerTest extends PHPUnit_Framework_TestCase
 {
