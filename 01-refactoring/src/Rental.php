2f41447b9591e719f39aa28c797d9bc008def4ff
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2016-11-04 21:42:08 +0100

    01: Fixing Code to PSR-2 with php-cs-fixer

--- 01-refactoring/src/Rental.php
+++ 01-refactoring/src/Rental.php
@@ -2,19 +2,22 @@
 
 namespace Refactoring;
 
-class Rental {
+class Rental
+{
+    private $_movie;
+    private $_daysRented;
 
-private  $_movie;
-private $_daysRented;
-
-function __construct($movie, $daysRented) {
-    $this->_movie = $movie;
-    $this->_daysRented = $daysRented;
-}
-public function getDaysRented() {
-    return $this->_daysRented;
-}
-public function getMovie() {
-    return $this->_movie;
-}
+    public function __construct($movie, $daysRented)
+    {
+        $this->_movie = $movie;
+        $this->_daysRented = $daysRented;
+    }
+    public function getDaysRented()
+    {
+        return $this->_daysRented;
+    }
+    public function getMovie()
+    {
+        return $this->_movie;
+    }
 }
