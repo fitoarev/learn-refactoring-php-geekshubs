7540fd9b85247c83c5147821eed3c71cdf373e10
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2016-11-11 20:57:10 +0100

    01: Extract calculation of total amount

--- 01-refactoring/src/Customer.php
+++ 01-refactoring/src/Customer.php
@@ -24,15 +24,12 @@ class Customer
 
     public function statement()
     {
-        $totalAmount = 0;
         $frequentRenterPoints = 0;
         $rentals = $this->_rentals;
 
         $result = "Rental Record for " . $this->getName() . "\n";
 
         foreach ($rentals as $rental) {
-            $totalAmount += $rental->obtainCharge();
-
             $frequentRenterPoints = $frequentRenterPoints + $rental->calculateFrequentRenterPoints();
 
 
@@ -40,10 +37,25 @@ class Customer
             $result .= "\t" . $rental->getMovie()->getTitle() . "\t" . $rental->obtainCharge() . "\n";
         }
         //add footer lines
-        $result .= "Amount owed is " . $totalAmount . "\n";
+        $result .= "Amount owed is " . $this->calculateTotalAmount($rentals) . "\n";
         $result .= "You earned " . $frequentRenterPoints . " frequent renter points";
 
 
         return $result;
+    }
+
+    /**
+     * @param Rental[] $rentals
+     * @return float
+     */
+    private function calculateTotalAmount($rentals)
+    {
+        $totalAmount = 0;
+
+        foreach ($rentals as $rental) {
+            $totalAmount += $rental->obtainCharge();
+        }
+
+        return $totalAmount;
     }
 }
