93e7bbedc7f1d12bc9723eea55928522b16289ff
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2016-11-15 19:42:20 +0100

    01: Cleaning obtainCharge of Children

--- 01-refactoring/src/Price/Children.php
+++ 01-refactoring/src/Price/Children.php
@@ -17,12 +17,11 @@ class Children extends Price
      */
     public function obtainCharge($daysRented)
     {
-        $thisAmount = 0;
-        $thisAmount += 1.5;
+        $amount = 1.5;
         if ($daysRented > 3) {
-            $thisAmount += ($daysRented - 3) * 1.5;
+            $amount = $amount + ($daysRented - 3) * 1.5;
         }
 
-        return $thisAmount;
+        return $amount;
     }
 }
