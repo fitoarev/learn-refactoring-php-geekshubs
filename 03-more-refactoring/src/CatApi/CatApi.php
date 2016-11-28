42611cbb69649edb6e58da2dc641a1d0d6187618
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2016-11-28 19:40:24 +0100

    03-reto: 06 else statement deletion

--- 03-more-refactoring/src/CatApi/CatApi.php
+++ 03-more-refactoring/src/CatApi/CatApi.php
@@ -10,30 +10,32 @@ class CatApi
     {
         if ($this->isInCache()) {
             return file_get_contents($this->cacheFilePath);
-        } else {
-            $responseXml = @file_get_contents(
-                'http://thecatapi.com/api/images/get?format=xml&type=jpg'
-            );
-            if (!$responseXml) {
-                // the cat API is down or something
-                return 'http://cdn.my-cool-website.com/default.jpg';
-            }
-
-            $responseElement = new \SimpleXMLElement($responseXml);
-
-            file_put_contents(
-                $this->cacheFilePath,
-                (string)$responseElement->data->images[0]->image->url
-            );
-
-            return (string)$responseElement->data->images[0]->image->url;
         }
+
+        $responseXml = @file_get_contents(
+            'http://thecatapi.com/api/images/get?format=xml&type=jpg'
+        );
+
+        if (!$responseXml) {
+            // the cat API is down or something
+            return 'http://cdn.my-cool-website.com/default.jpg';
+        }
+
+        $responseElement = new \SimpleXMLElement($responseXml);
+
+        file_put_contents(
+            $this->cacheFilePath,
+            (string)$responseElement->data->images[0]->image->url
+        );
+
+        return (string)$responseElement->data->images[0]->image->url;
     }
 
     private function isInCache()
     {
-        return ! $this->isNotInCache();
+        return !$this->isNotInCache();
     }
+
     /**
      * @return bool
      */
